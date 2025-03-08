25fb028af61c0ec0ffe5987cc235945e8d7e944b
Max Meekhoff <meekhoffm@gmail.com>
2025-03-08 14:52:57 -0500

    converting project to vite, tailwind 4, upgrading packages

---
+++ vite.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+	plugins: [vue(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
+});
